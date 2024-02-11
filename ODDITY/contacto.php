48332ee48eaa581bf85cce0d5d2f1075e5846f6c
Milton Denis Castro <159483393+MiltonCastro93@users.noreply.github.com>
2024-02-11 12:35:16 -0300

    Add files via upload

---
+++ ODDITY/contacto.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <header>
+        <a href="index.html">
+            <img src="imgs/logo.png" alt="Logo de Oddity">
+        </a>
+        
+        <nav>
+            <a href="index.html">Inicio</a>
+            <a href="">Servicios</a>
+            <a href="promos.html">Promos</a>
+            <a href="contactos.html">Contactos</a>
+        </nav>
+    </header>
+
+    <main>
+        <section id="contacto">
+            <article>
+                
+            </article>
+        </section>
+    </main>
+
+    <footer>
+        <a href="index.html"><img src="imgs/log-footer.png" alt=""></a>
+        <div>
+            <a href="#">
+                <img src="imgs/Instagram.png" alt="">
+            </a>
+            <a href="#">
+                <img src="imgs/facebook.png" alt="x">
+            </a>
+            <a href="#">
+                <img src="imgs/twitter.png" alt="">
+            </a>
+            <a href="#">
+                <img src="imgs/x.png" alt="">
+            </a>
+        </div>
+    </footer>
+
+</body>
+</html>
